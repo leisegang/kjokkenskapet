99f11f9dd9ae146dacef55868912097d929688d0
leisegang <leisegang@gmail.com>
2015-02-13 15:33:41 +0100

    navbar edit

--- wp-content/themes/kjokkenskapet/header.php
+++ wp-content/themes/kjokkenskapet/header.php
@@ -22,40 +22,29 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'kjokkenskapet' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+		<div class="container site-branding">
+        
+        <?php if ( get_header_image() ) : ?>
+			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" class="header-image">
+		<?php endif; // End header image check. ?>
+        
+       <div class="five columns branding">
+		<?php if ( of_get_option('logo_uploader') ) : ?>
+       		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="site-logo" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"> 		
+            <img src="<?php echo of_get_option('logo_uploader'); ?>" alt="<?php echo esc_attr( bloginfo( 'name' )); ?>"></a></h1>
+		<?php else : ?>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
-
-
-<!-- Top Menu -->
-	<nav class="<?php echo apply_filters( 'alienship_top_navbar_class' , 'navbar navbar-inverse top-navigation' ); ?>" role="navigation">
-		<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-		</div>
-
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<?php wp_nav_menu( array(
-				'theme_location' => 'top',
-				'depth'          => 2,
-				'container'      => false,
-				'menu_class'     => 'nav navbar-nav',
-				'walker'         => new wp_bootstrap_navwalker(),
-				'fallback_cb'    => 'wp_bootstrap_navwalker::fallback'
-				)
-			); ?>
-		</div>
-	</div>
-	</nav>
-<!-- End Top Menu -->
-
+            <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		<?php endif; ?>
+      </div> <!-- .five columns branding--> 
+      <div class="eleven columns navmenu">
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+			<h1 class="menu-toggle"><?php _e( 'Menu', 'impresa' ); ?></h1>
+			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'impresa' ); ?></a>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #site-navigation -->
+      </div> <!-- .eleven columns navmenu-->
+      </div><!-- .container site-branding-->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
