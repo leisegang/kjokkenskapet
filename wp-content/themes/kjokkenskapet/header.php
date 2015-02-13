d538620fd6fe7e70966657be81d7fdb8028f8792
leisegang <leisegang@gmail.com>
2015-02-13 18:59:11 +0100

    moved menu up

--- wp-content/themes/kjokkenskapet/header.php
+++ wp-content/themes/kjokkenskapet/header.php
@@ -22,22 +22,22 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-	
-	<div id="headimg">
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
-		<?php else : ?>
-		<h1 class="displaying-header-text"><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div class="displaying-header-text" id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
-		<?php endif; ?>
-	</div>
+       <nav id="site-navigation" class="main-navigation" role="navigation">
+           <h1 class="menu-toggle"><?php _e( 'Menu', 'kjokkenskapet' ); ?></h1>
+           <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'kjokkenskapet' ); ?></a>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'kjokkenskapet' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'kjokkenskapet' ); ?></a>
+           <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+       </nav><!-- #site-navigation -->
+       <div id="headimg">
+           <?php if ( get_header_image() ) : ?>
+           <img src="<?php header_image(); ?>" alt="">
+           <?php else : ?>
+           <h1 class="displaying-header-text"><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+           <div class="displaying-header-text" id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
+           <?php endif; ?>
+       </div>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
