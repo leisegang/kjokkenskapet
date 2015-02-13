26a846d08b8d4f5010ec9ec7809685b4ecd8b9fd
leisegang <leisegang@gmail.com>
2015-02-13 15:22:17 +0100

    navbar

--- wp-content/themes/kjokkenskapet/header.php
+++ wp-content/themes/kjokkenskapet/header.php
@@ -27,10 +27,35 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation navbar" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'kjokkenskapet' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+
+<!-- Top Menu -->
+	<nav class="<?php echo apply_filters( 'alienship_top_navbar_class' , 'navbar navbar-inverse top-navigation' ); ?>" role="navigation">
+		<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+		</div>
+
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<?php wp_nav_menu( array(
+				'theme_location' => 'top',
+				'depth'          => 2,
+				'container'      => false,
+				'menu_class'     => 'nav navbar-nav',
+				'walker'         => new wp_bootstrap_navwalker(),
+				'fallback_cb'    => 'wp_bootstrap_navwalker::fallback'
+				)
+			); ?>
+		</div>
+	</div>
+	</nav>
+<!-- End Top Menu -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
