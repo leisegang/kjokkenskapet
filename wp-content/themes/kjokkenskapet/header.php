1a9d6689418a428d8d2f77b94287ab939e088b0f
leisegang <leisegang@gmail.com>
2015-02-13 15:46:00 +0100

    header logo

--- wp-content/themes/kjokkenskapet/header.php
+++ wp-content/themes/kjokkenskapet/header.php
@@ -24,11 +24,16 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="container site-branding">
         
-	<?php if ( get_header_image() ) : ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-	</a>
-	<?php endif; // End header image check. ?>
+<?php if ( get_theme_mod( 'themeslug_logo' ) ) : >
+    <div class='site-logo'>
+        <a href='<?php echo esc_url( home_url( '/' ) ); >' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); >' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
+    </div>
+<?php else : ?>
+    <hgroup>
+        <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
+        <h2 class='site-description'><?php bloginfo( 'description' ); ?</h2>
+    </hgroup>
+<?php endif; ?>
       </div> <!-- .five columns branding--> 
       <div class="eleven columns navmenu">
         <nav id="site-navigation" class="main-navigation" role="navigation">
